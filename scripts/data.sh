8d478cf14af5e8a3a336a08a324f333234d03fa2
Gema <gem.anggada@gmail.com>
2019-11-07 10:23:23 +0700

    ---CHANGES:---   M scripts/data.sh ?? git/

--- scripts/data.sh
+++ scripts/data.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-files=("$HOME/.vimrc" "$HOME/.config/nvim/init.vim" "$HOME/.zshrc")
-repo_files=("./vim/.vimrc" "./vim/init.vim" "./zsh/.zshrc" )
+files=("$HOME/.vimrc" "$HOME/.config/nvim/init.vim" "$HOME/.zshrc" "$HOME/.gitconfig")
+repo_files=("./vim/.vimrc" "./vim/init.vim" "./zsh/.zshrc" "./git/.gitconfig")
