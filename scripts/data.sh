57e6efd0d6b04da6eedf3bff0b40a5cfdbb49a1d
Gema <gem.anggada@gmail.com>
2019-11-11 15:35:42 +0700

    ---CHANGES:---   M scripts/data.sh ?? undotted/coc-settings.json

--- scripts/data.sh
+++ scripts/data.sh
@@ -5,12 +5,14 @@ UNDOTTED="./undotted"
 files=(
 "$HOME/.vimrc"
 "$HOME/.config/nvim/init.vim"
+"$HOME/.config/nvim/coc-settings.json"
 "$HOME/.zshrc"
 "$HOME/.gitconfig"
 )
 repo_files=(
 "$UNDOTTED/vimrc"
 "$UNDOTTED/init.vim"
+"$UNDOTTED/coc-settings.json"
 "$UNDOTTED/zshrc"
 "$UNDOTTED/gitconfig"
 )
