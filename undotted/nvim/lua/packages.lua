cd1219d582a7a387e2834717f667d8617bf4a311
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-10-10 06:13:52 +0700

    ---CHANGES:---   M scripts/data.sh  M undotted/init.lua  D undotted/nvim/lua/dashboard.lua  M undotted/nvim/lua/packages.lua ?? undotted/nvim/lua/conf_dashboard.lua

--- undotted/nvim/lua/packages.lua
+++ undotted/nvim/lua/packages.lua
@@ -8,6 +8,8 @@ require('packer').startup(function()
   use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
   use { 'junegunn/fzf', run = ":call fzf#install()" }
   use { 'junegunn/fzf.vim' }
+-- try telescope sometimes
+-- use 'nvim-telescope/telescope.nvim'
   use 'vim-airline/vim-airline'
   use 'vim-airline/vim-airline-themes'
   use 'joshdick/onedark.vim'
@@ -21,7 +23,7 @@ require('packer').startup(function()
   use('MunifTanjim/prettier.nvim')
   use {'nvim-treesitter/nvim-treesitter'}
   use "nvim-lua/plenary.nvim"
-  use 'folke/lsp-colors.nvim'
+  --use 'folke/lsp-colors.nvim'
   use 'rbgrouleff/bclose.vim'
   use 'francoiscabrol/ranger.vim'
 
