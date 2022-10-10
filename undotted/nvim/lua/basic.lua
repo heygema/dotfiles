ee8e884a0bb60d44187abf3cb433f2a0667018b8
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-10-10 17:10:04 +0700

    ---CHANGES:---   M undotted/nvim/lua/basic.lua

--- undotted/nvim/lua/basic.lua
+++ undotted/nvim/lua/basic.lua
@@ -27,5 +27,16 @@ vim.o.clipboard = "unnamedplus"
 --
 --vim.o.smartcase = true
 
+-- fucking legacy vimscript from old vimrc
+vim.cmd [[ set foldenable ]]
 vim.cmd [[ set ignorecase ]]
 vim.cmd [[ set smartcase ]]
+vim.cmd [[
+if has('folding')
+  if has('windows')
+    let &fillchars='vert: '           " less cluttered vertical window separators
+  endif
+  set foldmethod=indent               " not as cool as syntax, but faster
+  set foldlevelstart=99               " start unfolded
+endif
+]]
